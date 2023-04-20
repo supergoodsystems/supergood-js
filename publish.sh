6282cf428111286a599f5afe089d3ba752a56813
Alex Klarfeld <alex.klarfeld@gmail.com>
2023-04-20 07:23:01 -0700

    Updated publish script

--- publish.sh
+++ publish.sh
@@ -1,1 +1,7 @@
+curl -C - -0 https://raw.githubusercontent.com/supergoodsystems/docs/main/installing-clients/node.js.md > README.md
+git add README.md
+git commit -m "Update README.md"
 yarn run clean && npx tsc && npm version $1 && npm publish --access public
+git add .
+git commit -m "Published latest version"
+git push origin master
