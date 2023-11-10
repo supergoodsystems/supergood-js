943845d2a224e669469866828580f1e4a8fab9b0
kurpav <kurochka.p@gmail.com>
2023-11-10 18:57:08 +0200

    chore: update publish script

--- publish.sh
+++ publish.sh
@@ -3,10 +3,6 @@ VERSION_TYPE=${1:-patch};
 curl -C - -0 https://raw.githubusercontent.com/supergoodsystems/docs/312ce3cd836606105def68ebe19d0b9cfc6c5452/integrate-with-clients/node.js/README.md > README.md;
 git add README.md;
 git commit -m "Update README.md";
-yarn run clean;
-npx tsc;
+
 npm version $VERSION_TYPE;
-npm publish --access public;
-git add .;
-git commit -m "Published latest version";
-git push origin master;
+npm publish;
