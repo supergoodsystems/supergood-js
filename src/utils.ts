ddd16e089eb372142438d8dfeabdd7dfebf039eb
Steven Bunting <sbunting96@gmail.com>
2023-11-15 13:50:17 -0800

    Add library info to supergood-bound headers (#12)
    * add info to request header
    
    * rework for new datadog flow
    
    * remove old added comma
    
    * bump patch version, fix to new header paradigm
    
    ---------
    
    Co-authored-by: Steve Bunting <steve@supergood.ai>

--- src/utils.ts
+++ src/utils.ts
@@ -176,13 +176,16 @@ function post(
   authorization: string
 ): Promise<string> {
   const dataString = JSON.stringify(data);
+  const packageVersion = version;
 
   const options = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Content-Length': dataString.length,
-      Authorization: authorization
+      Authorization: authorization,
+      'supergood-api': 'supergood-js',
+      'supergood-api-version': packageVersion
     },
     timeout: 5000 // in ms
   };
