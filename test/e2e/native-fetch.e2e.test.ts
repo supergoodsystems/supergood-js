cc457e3c3c1193eb05cf8a01ea2195650ff10581
kurpav <kurochka.p@gmail.com>
2023-12-14 16:08:54 +0200

    test: update libraries tests

--- test/e2e/native-fetch.e2e.test.ts
+++ test/e2e/native-fetch.e2e.test.ts
@@ -5,7 +5,7 @@ import {
   SUPERGOOD_CLIENT_SECRET,
   SUPERGOOD_SERVER
 } from '../consts';
-import { getEvents } from '../utils/function-call-args';
+import { checkPostedEvents } from '../utils/function-call-args';
 import { mockApi } from '../utils/mock-api';
 
 const [major] = process.versions.node.split('.').map(Number);
@@ -31,10 +31,11 @@ describeIf('native fetch', () => {
     expect(response.status).toEqual(200);
     await Supergood.close();
 
-    const eventsPosted = getEvents(postEventsMock);
-
-    expect(eventsPosted.length).toEqual(1);
-    expect(eventsPosted[0].response.body).toEqual(responseBody);
+    checkPostedEvents(postEventsMock, 1, {
+      response: expect.objectContaining({
+        body: responseBody
+      })
+    });
   });
 
   it('POST /posts', async () => {
@@ -50,10 +51,11 @@ describeIf('native fetch', () => {
     expect(response.status).toEqual(201);
     await Supergood.close();
 
-    const eventsPosted = getEvents(postEventsMock);
-
-    expect(eventsPosted[0].request.body).toEqual(body);
-    expect(eventsPosted[0].response.body).toEqual(responseBody);
-    expect(eventsPosted.length).toEqual(1);
+    checkPostedEvents(postEventsMock, 1, {
+      response: expect.objectContaining({
+        body: responseBody
+      }),
+      request: expect.objectContaining({ body })
+    });
   });
 });
