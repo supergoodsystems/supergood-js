07689d3434947f309a7c1548920b7482e9833b3d
kurpav <kurochka.p@gmail.com>
2024-01-09 17:12:47 +0200

    test: fix formatting

--- test/e2e/telemetry.e2e.test.ts
+++ test/e2e/telemetry.e2e.test.ts
@@ -1,6 +1,7 @@
+import axios from 'axios';
+
 import Supergood from '../../src';
 import { mockApi } from '../utils/mock-api';
-import axios from 'axios';
 
 import {
   MOCK_DATA_SERVER,
@@ -13,6 +14,7 @@ import { getTelemetry } from '../utils/function-call-args';
 
 describe('telemetry posting', () => {
   const { postTelemetryMock } = mockApi();
+
   it('should accurately post telemetry', async () => {
     await Supergood.init(
       {
@@ -20,17 +22,17 @@ describe('telemetry posting', () => {
         clientId: SUPERGOOD_CLIENT_ID,
         clientSecret: SUPERGOOD_CLIENT_SECRET,
         metadata: {
-          serviceName: "test-service-name",
+          serviceName: 'test-service-name'
         }
       },
       SUPERGOOD_SERVER
     );
     await axios.get(`${MOCK_DATA_SERVER}/posts`);
     await Supergood.close();
-    const { cacheKeys, cacheSize, serviceName } = getTelemetry(postTelemetryMock);
+    const { cacheKeys, cacheSize, serviceName } =
+      getTelemetry(postTelemetryMock);
     expect(cacheKeys).toEqual(1);
     expect(cacheSize).toEqual(160);
-    expect(serviceName).toEqual("test-service-name");
-  })
-
-})
+    expect(serviceName).toEqual('test-service-name');
+  });
+});
